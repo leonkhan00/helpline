4ee0efbd02ad55d54f30c4f63a2b12953b2a75c2
Leonkhan <leonkhan75369@gmail.com>
2025-08-29 22:02:25 +0600

    array for data created and calling functionlity added

--- script.js
+++ script.js
@@ -11,3 +11,64 @@ for (const heartIcon of heartIcons) {
     heartIcon.style.color = "red";
   });
 }
+
+// array for store history
+let historyDatas = [];
+const historyContainer = document.getElementById("historyContainer");
+// functionlity for call button
+const callBtns = document.querySelectorAll(".callBtn");
+const coin = document.getElementById("coin");
+for (const callBtn of callBtns) {
+  callBtn.addEventListener("click", function (e) {
+    // coin count
+
+    const totalCoin = parseInt(coin.innerText);
+    const coinLeft = totalCoin - 20;
+    if (coinLeft < 0) {
+      alert(
+        "You dont have enough coins for call. You need to have 20 coins atleast. "
+      );
+      return;
+    }
+    coin.innerText = coinLeft;
+    // calling
+    const serviceName =
+      callBtn.parentElement.previousElementSibling.previousElementSibling
+        .lastElementChild.innerText;
+    const serviceTitle =
+      callBtn.parentElement.previousElementSibling.previousElementSibling
+        .firstElementChild.innerText;
+    console.log(serviceTitle);
+    const serviceNum =
+      callBtn.parentElement.previousElementSibling.firstChild.nextSibling
+        .innerText;
+    alert(`Calling ${serviceName} ${serviceNum}`);
+    // set data
+    const data = {
+      name: serviceTitle,
+      number: serviceNum,
+      date: new Date().toLocaleTimeString(),
+    };
+    historyDatas.push(data);
+
+    // push data to history section
+    historyContainer.innerText = "";
+    for (const historyData of historyDatas) {
+      const div = document.createElement("div");
+      div.innerHTML = `
+        <div
+            class="flex justify-between items-center w-full h-auto bg-gray-100 px-3 py-2 mb-3 rounded-[10px]"
+          >
+            <div>
+              <h3 class="text-black font-semibold">${historyData.name}</h3>
+              <span class="text-gray-600">${historyData.number}</span>
+            </div>
+            <div>
+              <p>${historyData.date}</p>
+            </div>
+          </div>
+      `;
+      historyContainer.appendChild(div);
+    }
+  });
+}
